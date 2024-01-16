1ae762068fef390d36e6ba1ef3cda19c4db3daa0
이승재 <152847551+tmdwo10@users.noreply.github.com>
2024-01-16 12:41:19 +0900

    Update web_server.py

--- project_Bello_raspi/web_server.py
+++ project_Bello_raspi/web_server.py
@@ -1,5 +1,6 @@
 # web_server.py
 # /home/bello/project_env/bin/python /home/bello/project_test/web_server.py
+
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_mysqldb import MySQL
 from flask_cors import CORS
@@ -23,6 +24,7 @@ app.config['MYSQL_DB'] = 'campus_23K_SG4_hack_3'
 mysql = MySQL(app)
 audio = alsaaudio.Mixer()
 
+#LCDpage 웹소켓으로 이전
 @app.route('/')
 def LCDpage():
     #session['user_id'] = 'test0110' #test용 세션
